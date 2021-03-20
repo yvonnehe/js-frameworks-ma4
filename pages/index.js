a6c821584d5a7a7cdb6b02ca6f4fd62c859f2e2c
Yvonne H <yvonnehelland@outlook.com>
2021-03-20 16:20:59 +0100

    Adds link

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Head from "next/head";
 import Heading from "../components/Heading";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
     <>
       <Heading heading="Home page" />
-      <p>Check out these really cool results: *link</p>
+      <p>
+        Check out these really cool{" "}
+        <Link className="deco" href="/results">
+          results
+        </Link>
+      </p>
     </>
   );
 };
